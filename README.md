715d26705ae67ecc6a6aad6cbfc58ec6d385eda4
Radoslav Petkov <radipirate@gmail.com>
2018-10-19 00:24:54 +0300

    impl recursive resizing update readme

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 This is simple script used for up/down scaling images.
 It supports processing both single images and folders.
+When a folder is passed the script recursively traverse it, resize images and
+create directories thus preserving the directory structure.
 
 # How to use it?
 
-* To resize an image run `python3 resize.py 0.25 path_to_image`
-* To resize folder `python3 resize.py 0.25 path_to_folder`
+run `python3 resize.py -h`
+
+# Examples
+* To resize an image run `python3 resize.py 0.25 path_to_image output_folder`
+* To resize folder `python3 resize.py 0.25 path_to_folder output_folder`
--- resize.py
+++ resize.py
@@ -2,29 +2,60 @@ def main():
     args = get_args()
 
     import os
-    prefix = 'resized_'
     if args.nprefix:
         prefix = args.nprefix
+    else:
+        prefix = 'resized_{}_'.format(args.ratio)
 
     if os.path.isdir(args.impath):
         image_paths = [os.path.join(args.impath, image_name) for image_name in os.listdir(args.impath)]
     else:
         image_paths = [args.impath]
 
-    if args.out:
-        output_folder = args.out
-    else:
-        output_folder = get_path_without_image_name(image_paths[0])
+    resize_image_paths(image_paths, args.ratio, prefix, args.out)
 
-    if not os.path.exists(output_folder):
-        os.mkdir(output_folder)
 
+def resize_image_paths(image_paths, ratio, prefix, root_output_folder):
+    import os
+
+    def dfs(current_image_path, visited, current_output_folder):
+        if current_image_path in visited:
+            return
+        else:
+            visited.add(current_image_path)
+
+        if not os.path.exists(current_output_folder):
+            os.mkdir(current_output_folder)
+
+        if not os.path.isdir(current_image_path):
+            resize_save_image(current_image_path,
+                              ratio,
+                              get_new_image_path(
+                                  current_output_folder,
+                                  get_image_name_from_path(current_image_path),
+                                  prefix))
+        else:
+            new_output_folder = os.path.join(current_output_folder, get_last_path_node(current_image_path))
+            for image_name in os.listdir(current_image_path):
+                dfs(os.path.join(current_image_path, image_name), visited, new_output_folder)
+
+    visited = set()
+
+    if not os.path.exists(root_output_folder):
+        os.mkdir(root_output_folder)
+
+    root_input_folder = get_basename(image_paths[0])
     for image_path in image_paths:
-        resize_save_image(image_path,
-                          args.ratio,
-                          get_new_image_path(output_folder,
-                                             get_image_name_from_path(image_path),
-                                             prefix))
+        dfs(image_path, visited, os.path.join(root_output_folder, root_input_folder))
+
+
+def get_last_path_node(path: str) -> str:
+    return path.rsplit('/', 1)[1]
+
+
+def get_basename(path):
+    import os
+    return os.path.basename(os.path.dirname(path))
 
 
 def get_args():
@@ -33,9 +64,9 @@ def get_args():
 
     parser.add_argument('ratio', help='Ratio to multiply width and height', type=float)
     parser.add_argument('impath', help='Folder or path to image', type=str)
+    parser.add_argument('out', help='Folder to output files in', type=str)
 
-    parser.add_argument('--nprefix', help='Prefix to append to the image name of the resized image', type=str)
-    parser.add_argument('--out', help='Folder to output files in', type=str)
+    parser.add_argument('--nprefix', help='Prefix appended to the filename of the resized image', type=str)
 
     return parser.parse_args()
 
@@ -54,7 +85,7 @@ def resize_save_image(image_path: str, ratio: float, new_image_path: str):
 
 
 def get_image_name_from_path(image_path: str) -> str:
-    return image_path.rsplit('/', 1)[1]
+    return get_last_path_node(image_path)
 
 
 def get_path_without_image_name(image_path: str) -> str:
